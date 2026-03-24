fa3d25b09e0045b20b8681837c85bea5f0002a99
tivvvv <18612553935@163.com>
2026-03-24 23:08:27 +0800

    feat: 配置项目.

---
+++ src/main/java/com/tiv/bi/Application.java
@@ -0,0 +1,19 @@
+package com.tiv.bi;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        // 加载.env文件
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        // 设置.env文件中的变量参数
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
+        SpringApplication.run(Application.class, args);
+    }
+
+}
