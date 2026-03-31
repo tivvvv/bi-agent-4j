606f78955acec79e6d19c45afda897ea18d8b4fe
tivvvv <18612553935@163.com>
2026-03-31 13:27:07 +0800

    feat: 集成Redis Stack作为向量数据库.

--- src/main/java/com/tiv/bi/service/impl/DocumentServiceImpl.java
+++ src/main/java/com/tiv/bi/service/impl/DocumentServiceImpl.java
@@ -1,8 +1,10 @@
 package com.tiv.bi.service.impl;
 
 import com.tiv.bi.service.DocumentService;
+import jakarta.annotation.Resource;
 import org.springframework.ai.document.Document;
 import org.springframework.ai.reader.tika.TikaDocumentReader;
+import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -11,11 +13,17 @@ import java.util.List;
 @Service
 public class DocumentServiceImpl implements DocumentService {
 
+    @Resource
+    private VectorStore vectorStore;
+
     @Override
     public void handleDocument(MultipartFile multipartFile) {
         // 1. 读取文档
         TikaDocumentReader tikaDocumentReader = new TikaDocumentReader(multipartFile.getResource());
         List<Document> documents = tikaDocumentReader.get();
+
+        // 2. 入库
+        vectorStore.add(documents);
     }
 
 }
